59888e0e5a9719699c9c359a1b6881d37becd6c1
Blake Haswell <haswell00@gmail.com>
2014-03-25 17:14:34 +1100

    Added TODO reminder to change behaviour of initialBoard function.

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 // TODO: Randomise position of starting square. –BH 2014/03/23
-exports.initialBoard = function() {
+// TODO: Remove starting square, should return a completely empty board. Replace
+//       instead with an `insertSquare` function.
+exports.initialBoard = function () {
     var board = [];
     for (var i = 0; i < 16; i++) {
         board[i] = 0;
