30794be22488eb35eeeb7bde2d7a27b204282eae
Blake Haswell <haswell00@gmail.com>
2014-03-27 20:08:02 +1100

    Removed starting square from initialBoard function so that it can be replaced by a generic insert square function.

--- index.js
+++ index.js
@@ -5,15 +5,9 @@ var zip = require('underscore').zip;
 var partial = require('underscore').partial;
 var compose = require('underscore').compose;
 
-// TODO: Randomise position of starting square. –BH 2014/03/23
-// TODO: Remove starting square, should return a completely empty board. Replace
-//       instead with an `insertSquare` function.
 exports.initialBoard = function () {
     var board = [];
-    for (var i = 0; i < 16; i++) {
-        board[i] = 0;
-    }
-    board[0] = 2; // Sets starting square.
+    for (var i = 0; i < 16; i++) board[i] = 0;
     return board;
 };
 
--- test/initialBoard.js
+++ test/initialBoard.js
@@ -2,9 +2,7 @@ var describe = require('mocha').describe;
 var beforeEach = require('mocha').beforeEach;
 var it = require('mocha').it;
 var expect = require('chai').expect;
-var each = require('underscore').each;
 var filter = require('underscore').filter;
-var isNumber = require('underscore').isNumber;
 var game = require('../index');
 
 describe('initialBoard()', function () {
@@ -13,21 +11,11 @@ describe('initialBoard()', function () {
         this.board = game.initialBoard();
     });
 
-    it('should return an array with 16 numbers', function () {
+    it('should return an array with 16 zeros', function () {
+        expect(this.board).to.be.instanceOf(Array);
         expect(this.board.length).to.equal(16);
-        each(this.board, function (value) {
-            expect(isNumber(value)).to.equal(true);
-        });
-    });
-
-    it('should return an array with 15 zeros', function () {
         var zeros = filter(this.board, function (value) { return 0 === value; });
-        expect(zeros.length).to.equal(15);
-    });
-
-    it('should return an array with 1 two', function () {
-        var twos = filter(this.board, function (value) { return 2 === value; });
-        expect(twos.length).to.equal(1);
+        expect(zeros.length).to.equal(16);
     });
 
 });
